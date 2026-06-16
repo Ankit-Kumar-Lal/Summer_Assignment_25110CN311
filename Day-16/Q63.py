1b7ea0250ee81369c682f170ff5b7c512b7737f8
Ankit Kumar Lal <ankitkumarlal2006@gmail.com>
2026-06-16 23:24:45 +0530

    Day-16 assignment is uploaded

--- Day-16/Q63.py
+++ Day-16/Q63.py
@@ -1,28 +1,13 @@
 # Write a program to Find pair with given sum
 
-# arr = [1, 4, 5, 6, 8, 2]
-# target = 10
-
-# found = False
-
-# for i in range(len(arr)):
-#     for j in range(i + 1, len(arr)):
-#         if arr[i] + arr[j] == target:
-#             print("Pair found:", arr[i], arr[j])
-#             found = True
-
-# if found == False:
-#     print("No pair found")
-
 arr = [1, 4, 5, 6, 8, 2]
 target = 10
 
-# Count elements manually
 n = 0
 for x in arr:
     n = n + 1
 
-# Find pairs
+# Finding pairs
 for i in range(n):
     for j in range(i + 1, n):
         if arr[i] + arr[j] == target:
